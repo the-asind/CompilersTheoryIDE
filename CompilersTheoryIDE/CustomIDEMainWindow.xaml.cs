364b732fdfba06c113cd59d453a8eeab88d26660
the-asind <84527186+the-asind@users.noreply.github.com>
2024-02-18 22:39:04 +0700

    Initial commit

---
+++ CompilersTheoryIDE/CustomIDEMainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace YourNamespace
+{
+    public partial class CustomIDEMainWindow : Window
+    {
+        public CustomIDEMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Пример метода для обработки нажатия кнопки "Открыть"
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Загрузите текст из файла и отобразите его в редакторе
+                // Пример:
+                // TextBoxYourCodeEditor.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        // Пример метода для обработки нажатия кнопки "Сохранить"
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                // Сохраните текст из редактора в файл
+                // Пример:
+                // File.WriteAllText(saveFileDialog.FileName, TextBoxYourCodeEditor.Text);
+            }
+        }
+
+        // Тут могут быть методы для других кнопок и функций
+
+    }
+}
